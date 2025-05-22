860eeb78b76c88d3bc00a542ad34a4a8743f3ff4
mgonzalez <mgonzalez@login-3.cnca>
2025-05-22 07:08:13 -0600

    training args

--- train_univ.py
+++ train_univ.py
@@ -81,11 +81,11 @@ def parse_args():
                         help="Early stopping delta")
     parser.add_argument("--do_wandb", action="store_true", default=False,
                         help="Enable Weights & Biases logging")
-    parser.add_argument("--constant_lr", action="store_false", default=True,
+    parser.add_argument("--constant_lr", action="store_true", default=False,
                         help="Enable Learning rate decay")
     parser.add_argument("--non-uniform_sampling", action="store_true", default=False,
                         help="Enable non-uniform sampling")
-    parser.add_argument("--attention", action="store_false", default=True,
+    parser.add_argument("--attention", action="store_true", default=False,
                         help="Use attention in the diffusion model")
     parser.add_argument("--run_name", type=str, default=None,
                         help="W&B run name (optional)")
